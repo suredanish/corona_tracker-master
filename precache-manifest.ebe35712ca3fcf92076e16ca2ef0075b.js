cae0dbc99e6fcf92b382a1d05f4438128125e215
suredanish <suredanish@gmail.com>
2021-06-07 16:02:03 +0530

    Updates

---
+++ precache-manifest.ebe35712ca3fcf92076e16ca2ef0075b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0aba7b6467c4b9f45fb81c22b6abe2",
+    "url": "/corona_tracker-master/index.html"
+  },
+  {
+    "revision": "c3273ba4d2eac6193f23",
+    "url": "/corona_tracker-master/static/css/main.919b4191.chunk.css"
+  },
+  {
+    "revision": "bcb8135d5a4537181115",
+    "url": "/corona_tracker-master/static/js/2.0f8d03e0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_tracker-master/static/js/2.0f8d03e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3273ba4d2eac6193f23",
+    "url": "/corona_tracker-master/static/js/main.bae69323.chunk.js"
+  },
+  {
+    "revision": "662151dea0464a366f98",
+    "url": "/corona_tracker-master/static/js/runtime-main.c6a2c5ea.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_tracker-master/static/media/image.d7265326.png"
+  }
+]);
